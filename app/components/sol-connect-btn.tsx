01d070c20034249c6934aeeddb4bd178c4f17023
Mantas Imb <MantasImbrasas@gmail.com>
2024-02-17 16:02:38 +0200

    first commit

---
+++ app/components/sol-connect-btn.tsx
@@ -0,0 +1,11 @@
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+
+export default function SolConnectBtn() {
+  const { setVisible } = useWalletModal();
+
+  const connect = () => {
+    setVisible(true);
+  };
+
+  return <button onClick={connect}>Connect wallet</button>;
+}
---
+++ app/components/sol-providers.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  LedgerWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+// import the styles
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+export default function SolanaProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // you can use Mainnet, Devnet or Testnet here
+  const solNetwork = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(solNetwork), [solNetwork]);
+  // initialise all the wallets you want to use
+  const wallets = useMemo(
+    () => [
+      new SolflareWalletAdapter({ network: solNetwork }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+    ],
+    [solNetwork]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
